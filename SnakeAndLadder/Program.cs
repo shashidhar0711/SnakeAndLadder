eb4761a1c40b3bcb69667ba38bc4b0866e2e8cd1
shashidhar0711 <shashidhar.sasic@gmail.com>
2020-10-23 12:38:40 +0530

    Add project files.

---
+++ SnakeAndLadder/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    class Program
+    {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        static void Main(string[] args)
+        {
+            /// Printing Welcome Message
+            Console.WriteLine("Welcome to Snake and Ladder Simulator");
+        }
+    }
+}
