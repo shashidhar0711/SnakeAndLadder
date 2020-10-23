127fe760f0feab525cf925c7298856326ac57b99
shashidhar0711 <shashidhar.sasic@gmail.com>
2020-10-23 16:03:00 +0530

    [sasic] Add . Repeat Till Player Reaches Winning Position

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -4,14 +4,71 @@ namespace SnakeAndLadder
 {
     class Program
     {
-        /// <summary>
-        /// Defines the entry point of the application.
-        /// </summary>
-        /// <param name="args">The arguments.</param>
+        const int START_POSITION = 0;
+        const int END_POSITION = 100;
+
+        /// Create Reference for Random class
+        static Random random = new Random();
+
+        /// variables
+        public static int currentPosition = 0;
+        public static int selectedOption = 0;
+
         static void Main(string[] args)
         {
-            /// Printing Welcome Message
-            Console.WriteLine("Welcome to Snake and Ladder Simulator");
+            /// Computation
+            Console.WriteLine("Current Position is : " + PlayerOption());
+        }
+
+        /// <summary>
+        /// Players the option.
+        /// </summary>
+        /// <returns></returns>
+        public static int PlayerOption()
+        {
+            /// If Selected Option is 3(In case Snake), 
+            /// Player Current Postion Moves Behind by The Number of Position recieved in the Dice
+            /// If Option is 2(In case Ladder), Player Current Position Moves Ahead
+            /// If Option is 1(In case No Play),Player Stay in Same Position
+            while (currentPosition < END_POSITION - 1)
+            {
+                /// Variables
+                int thrownDice = ThrowDice();
+                int selectedOption = CheckForOption();
+
+                if (selectedOption == 2)
+                {
+                    currentPosition += thrownDice;
+                }
+                else if (selectedOption == 3)
+                {
+                    currentPosition -= thrownDice;
+
+                    /// If Current Position is Lessthan Zero then,
+                    /// Player Position Starts from STARTING_POSITION (ie, 0)
+                    if (currentPosition < 0)
+                    {
+                        currentPosition = START_POSITION;
+                    }                
+                }
+                else
+                {
+                    ; // Do Nothing Player Stay in Same Position
+                }
+            }
+            /// It returns Current Position
+            return currentPosition;
+        }
+        public static int ThrowDice()
+        {
+            /// It returns Random Number Between 1 to 6
+            return random.Next(1, 7);
+        }
+
+        public static int CheckForOption()
+        {
+            /// It returns Random Number Between 1 to 3
+            return random.Next(1, 4);
         }
     }
 }
