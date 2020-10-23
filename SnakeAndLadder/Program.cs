357bedca945029170cf1e84ed254cde5fb2dd094
shashidhar0711 <shashidhar.sasic@gmail.com>
2020-10-23 13:19:59 +0530

    [sasic] Add . Rolls the Die To Get a Random Number

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -4,14 +4,35 @@ namespace SnakeAndLadder
 {
     class Program
     {
+        const int START_POSITION = 0;
+        const int END_POSITION = 100;
+
+        /// variables
+        public static int currentPosition = 0;
+
         /// <summary>
         /// Defines the entry point of the application.
         /// </summary>
         /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
-            /// Printing Welcome Message
-            Console.WriteLine("Welcome to Snake and Ladder Simulator");
+            /// Computation
+            /// prints the Current Position
+            currentPosition += ThrowDice();
+            Console.WriteLine("Current position :"+currentPosition);
+        }
+
+        /// <summary>
+        /// Throws the dice.
+        /// </summary>
+        /// <returns></returns>
+        public static int ThrowDice()
+        {
+            /// Create Reference for Random class
+            Random random = new Random();
+
+            /// It Returns a Random value 
+            return random.Next(1, 7);
         }
     }
 }
