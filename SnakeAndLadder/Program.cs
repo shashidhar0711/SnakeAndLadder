ec10f9aadbedd4b948facaeac23efcee51c3a2b2
shashidhar0711 <shashidhar.sasic@gmail.com>
2020-10-23 12:54:08 +0530

    [sasic] Add . Initialize Constants of Position

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -4,14 +4,18 @@ namespace SnakeAndLadder
 {
     class Program
     {
+        const int START_POSITION = 0;
+        const int END_POSITION = 100;
         /// <summary>
         /// Defines the entry point of the application.
         /// </summary>
         /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
-            /// Printing Welcome Message
-            Console.WriteLine("Welcome to Snake and Ladder Simulator");
+            /// Prints Starting position and Ending position
+            Console.WriteLine("Starting Position : "+START_POSITION);
+            Console.WriteLine("Ending Position : " +END_POSITION);
+
         }
     }
 }
